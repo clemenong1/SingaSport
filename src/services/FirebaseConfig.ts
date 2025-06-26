4021541203062c55dda7fac3a87ad910576c493f
clemenong1 <clemenojk28@gmail.com>
2025-06-26 18:46:07 +0800

    secure api

--- src/services/FirebaseConfig.ts
+++ src/services/FirebaseConfig.ts
@@ -5,17 +5,25 @@ import { initializeApp } from "firebase/app";
 import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
+import { 
+  FIREBASE_API_KEY,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_APP_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECT_ID 
+} from '@env';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAmuuOXT6dRUStt4-8502jYXO2c25OfLwI",
-  authDomain: "singasport-cd006.firebaseapp.com",
-  projectId: "singasport-cd006",
-  storageBucket: "singasport-cd006.firebasestorage.app",
-  messagingSenderId: "166758109207",
-  appId: "1:166758109207:web:d12304df252ecf55364c52"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
